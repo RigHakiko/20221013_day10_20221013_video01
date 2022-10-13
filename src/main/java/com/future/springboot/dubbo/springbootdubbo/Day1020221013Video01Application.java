21c3653c594c191d5c42c63a36959fa1dff4d4d2
RigHakiko <add-122@hotmail.com>
2022-10-13 23:45:09 +0900

    Initial commit

---
+++ src/main/java/com/future/springboot/dubbo/springbootdubbo/Day1020221013Video01Application.java
@@ -0,0 +1,13 @@
+package com.future.springboot.dubbo.springbootdubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1020221013Video01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1020221013Video01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/future/springboot/dubbo/springbootdubbo/Day1020221013Video01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.springboot.dubbo.springbootdubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1020221013Video01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
